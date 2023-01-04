abe1388ba10b9e5f5ed70b325e722dc56cf91cea
Paulo Wender <paulowender2011@gmail.com>
2023-01-04 01:18:38 -0400

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const publicPath = path.join(__dirname, '..', 'public')
+app.use(express.static(publicPath))
+app.set('views', publicPath)
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+let messages = []
+
+io.on('connection', socket => {
+    console.log(`Client ${socket.id} connected`)
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data => {
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage',  data)
+    })
+})
+
+server.listen(3000, () => console.log('Server running on port 3000'))
